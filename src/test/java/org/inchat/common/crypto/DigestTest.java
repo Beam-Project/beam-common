b394a82e37c29d8fbc74658049fb942f8391fd06
Rene <rene.bernhardsgruetter@gmail.com>
2014-02-10 20:38:55 +0100

    Removed no longer necessary method. The message's integrity is completely guaranteed by ECIES.

--- src/test/java/org/inchat/common/crypto/DigestTest.java
+++ src/test/java/org/inchat/common/crypto/DigestTest.java
@@ -23,8 +23,6 @@ import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.Security;
 import org.bouncycastle.util.encoders.Hex;
-import org.inchat.common.Message;
-import org.inchat.common.Participant;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.Before;
@@ -34,13 +32,11 @@ public class DigestTest {
     private final static String REFERENCE_DIGEST_PROVIDER = "SUN";
     private final static String HELLO_LOWER_CASE_AS_SHA256 = "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824";
     private byte[] bytePayload;
-    private Message messagePayload;
     private byte[] output;
 
     @Before
     public void setUp() {
         bytePayload = "hello".getBytes();
-        messagePayload = new Message();
     }
 
     @Test
@@ -84,81 +80,6 @@ public class DigestTest {
 
             payloadText += textAddition;
         }
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testDigestWithSha256ByMessageOnNull() {
-        messagePayload = null;
-        output = Digest.digestWithSha256(messagePayload);
-    }
-
-    @Test
-    public void testDigestWithSha256ByMessageOnNotCompletelyFilledArgument() {
-        try {
-            output = Digest.digestWithSha256(messagePayload);
-            fail("Exception should have been thrown.");
-        } catch (IllegalArgumentException ex) {
-        }
-
-        try {
-            messagePayload.setVersion("1");
-            output = Digest.digestWithSha256(messagePayload);
-            fail("Exception should have been thrown.");
-        } catch (IllegalArgumentException ex) {
-        }
-
-        try {
-            Participant filledParticipant = new Participant(ParticipantIdGenerator.generateId());
-            messagePayload.setParticipant(filledParticipant);
-            output = Digest.digestWithSha256(messagePayload);
-            fail("Exception should have been thrown.");
-        } catch (IllegalArgumentException ex) {
-        }
-
-        try {
-            messagePayload.setInitializationVector(new byte[]{1, 2, 3});
-            output = Digest.digestWithSha256(messagePayload);
-            fail("Exception should have been thrown.");
-        } catch (IllegalArgumentException ex) {
-        }
-
-        try {
-            messagePayload.setKey(new byte[]{1, 2, 3});
-            output = Digest.digestWithSha256(messagePayload);
-            fail("Exception should have been thrown.");
-        } catch (IllegalArgumentException ex) {
-        }
-
-        messagePayload.setContent(new byte[]{1, 2, 3});
-        output = Digest.digestWithSha256(messagePayload); // nothing should be thrown here
-    }
-
-    @Test
-    public void testDigestWithSha256ByMessage() {
-        byte[] participantId = new byte[]{(byte) 104, (byte) 75, (byte) -86,
-            (byte) -75, (byte) -19, (byte) -112, (byte) -90, (byte) -10,
-            (byte) 10, (byte) -7, (byte) -114, (byte) 31, (byte) -113,
-            (byte) -78, (byte) 29, (byte) -82, (byte) -14, (byte) -36,
-            (byte) -89, (byte) -109, (byte) 118, (byte) 73, (byte) -12,
-            (byte) 27, (byte) 12, (byte) -69, (byte) 36, (byte) -51,
-            (byte) -79, (byte) -30, (byte) 89, (byte) 100};
-        byte[] expectedDigest = new byte[]{(byte) 27, (byte) 34, (byte) -21,
-            (byte) -113, (byte) 56, (byte) 1, (byte) -78, (byte) -23,
-            (byte) -26, (byte) -64, (byte) 81, (byte) -12, (byte) 53,
-            (byte) 93, (byte) 12, (byte) -115, (byte) -70, (byte) -90,
-            (byte) 42, (byte) -121, (byte) -96, (byte) -21, (byte) -37,
-            (byte) 126, (byte) -77, (byte) -90, (byte) -110, (byte) 117,
-            (byte) 120, (byte) 6, (byte) -127, (byte) -37};
-
-        messagePayload.setVersion("1.0");
-        messagePayload.setParticipant(new Participant(participantId));
-        messagePayload.setInitializationVector(new byte[]{1, 2, 3});
-        messagePayload.setKey(new byte[]{4, 5, 6});
-        messagePayload.setContent(new byte[]{7, 8, 9});
-
-        output = Digest.digestWithSha256(messagePayload);
-
-        assertArrayEquals(expectedDigest, output);
     }
 
 }
