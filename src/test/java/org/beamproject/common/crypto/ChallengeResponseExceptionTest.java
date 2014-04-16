9215d32d0a522f925402ffdcf5d14724f802d270
René Bernhardsgrütter <rene.bernhardsgruetter@posteo.ch>
2014-04-16 20:20:34 +0200

    Cleanup.

--- src/test/java/org/beamproject/common/crypto/ChallengeResponseExceptionTest.java
+++ src/test/java/org/beamproject/common/crypto/ChallengeResponseExceptionTest.java
@@ -23,10 +23,10 @@ import static org.junit.Assert.*;
 
 public class ChallengeResponseExceptionTest {
 
-
     @Test
     public void testConstructorOnAssignment() {
         String message = "hello world";
+        
         try {
             throw new ChallengeResponseException(message);
         } catch (ChallengeResponseException e) {
