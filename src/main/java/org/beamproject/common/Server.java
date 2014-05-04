2f73c98b7a2e4dbd6c5ffaa286ac3969a43fb07d
René Bernhardsgrütter <rene.bernhardsgruetter@posteo.ch>
2014-05-04 19:41:05 +0200

    Added .equals method and a constructor to create this server via its address (like beam:dsdlfe093w...).

--- src/main/java/org/beamproject/common/Server.java
+++ src/main/java/org/beamproject/common/Server.java
@@ -21,7 +21,9 @@ package org.beamproject.common;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.KeyPair;
+import java.util.Objects;
 import org.beamproject.common.crypto.EccKeyPairGenerator;
+import org.beamproject.common.util.Base58;
 import org.beamproject.common.util.Exceptions;
 
 /**
@@ -51,9 +53,68 @@ public class Server extends Participant {
 
         this.url = url;
     }
-    
+
+    /**
+     * Constructs a new {@link Server} from the given address.
+     * <p>
+     * The address has to be like
+     * {@code beam:[Base58 public key]?url=[Base58 URL]}.
+     *
+     * @param address The address to use.
+     * @throws IllegalArgumentException If the argument is not a valid address.
+     */
+    public Server(String address) {
+        this(extractUrlFromAddress(address), extractKeyPairFromAddress(address));
+    }
+
     public URL getUrl() {
         return url;
+    }
+
+    public String getAddress() {
+        return "beam:" + getPublicKeyAsBase58() + "?url=" + Base58.encode(url.toString().getBytes());
+    }
+
+    /**
+     * Compares this {@link Server} to the other object. The objects are only
+     * equals if both are of the same type, both have the same keys (both
+     * {@link PublicKey}s and/or both {@link PrivateKey}s) AND if they are
+     * pairwise the same, and if both URLs are equal. Two {@link Server}s are
+     * also the same, when both key pairs are null and the URLs are equal.
+     *
+     * @param other Another object to compare with this one.
+     * @return true, if the key pairs are equals, otherwise false.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+
+        if (other == null) {
+            return false;
+        }
+
+        if (other.getClass() != this.getClass()) {
+            return false;
+        }
+
+        Participant otherAsParticipant = (Participant) other;
+
+        if (!super.equals(otherAsParticipant)) {
+            return false;
+        }
+
+        Server otherAsServer = (Server) other;
+
+        return url.equals(otherAsServer.url);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 41 * hash + Objects.hashCode(this.url);
+        return hash;
     }
 
     /**
@@ -69,6 +130,36 @@ public class Server extends Participant {
             return new Server(new URL("http://example.com"), EccKeyPairGenerator.generate());
         } catch (MalformedURLException ex) {
             throw new IllegalStateException("Could not create a new server object: " + ex.getMessage());
+        }
+    }
+
+    static URL extractUrlFromAddress(String address) {
+        validateAddressFromat(address);
+
+        try {
+            String urlPart = address.substring(address.indexOf("?url=") + 5);
+            String urlAsString = new String(Base58.decode(urlPart));
+            return new URL(urlAsString);
+        } catch (MalformedURLException ex) {
+            throw new IllegalArgumentException("The URL of the address is invalid.");
+        }
+    }
+
+    static KeyPair extractKeyPairFromAddress(String address) {
+        validateAddressFromat(address);
+
+        try {
+            String keyPairPart = address.substring("beam:".length(), address.indexOf('?'));
+            return EccKeyPairGenerator.fromPublicKey(Base58.decode(keyPairPart));
+        } catch (IllegalStateException ex) {
+            throw new IllegalArgumentException("The public key of the address is invalid.");
+        }
+    }
+
+    static void validateAddressFromat(String address) {
+        if (address == null
+                || !address.matches("beam:[a-zA-Z0-9]+\\?url=[a-zA-Z0-9]+")) {
+            throw new IllegalArgumentException("The given address is not valild.");
         }
     }
 
--- src/test/java/org/beamproject/common/ServerTest.java
+++ src/test/java/org/beamproject/common/ServerTest.java
@@ -21,6 +21,8 @@ package org.beamproject.common;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.KeyPair;
+import org.beamproject.common.crypto.EccKeyPairGenerator;
+import org.beamproject.common.util.Base58;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.Before;
@@ -30,39 +32,125 @@ public class ServerTest {
     private Server server;
     private URL url;
     private KeyPair keyPair;
+    private String urlAsBase58;
 
     @Before
     public void setUp() throws MalformedURLException {
         server = Server.generate();
         url = server.url;
         keyPair = server.keyPair;
+        urlAsBase58 = Base58.encode(url.toString().getBytes());
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testConstructorOnNulls() {
+    public void testUrlKeyPairConstructorOnNulls() {
         server = new Server(null, null);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testConstructorOnNullUrl() {
+    public void testUrlKeyPairConstructorOnNullUrl() {
         server = new Server(null, keyPair);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testConstructorOnNullKeyPair() {
+    public void testUrlKeyPairConstructorOnNullKeyPair() {
         server = new Server(url, null);
     }
 
     @Test
-    public void testConstructor() {
+    public void testUrlKeyPairConstructor() {
         server = new Server(url, keyPair);
         assertSame(url, server.url);
         assertSame(keyPair, server.keyPair);
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddressContstructorOnNull() {
+        server = new Server(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddressContstructorOnEmptyString() {
+        server = new Server("");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddressContstructorOnInvalidAddress() {
+        server = new Server("beam:longTextLongTexturl=asdf");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddressContstructorOnIllegalChracters() {
+        server = new Server("beam\0\":)(*&^%$#@?url~~~!!@äåé®äåé®þäå®éáßðg\0asdf");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddressContstructorOnMissingPublicKey() {
+        server = new Server("beam:?url=" + urlAsBase58);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddressContstructorOnMissingUrl() {
+        server = new Server("beam:" + server.getPublicKeyAsBase58());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddressContstructorOnMalformedPublicKey() {
+        String malformedPublicKey = server.getPublicKeyAsBase58().replaceAll("a", "X");
+        server = new Server("beam:" + malformedPublicKey + "?url=" + urlAsBase58);
+    }
+
+    @Test
+    public void testAddressContstructor() {
+        Server created = new Server("beam:" + server.getPublicKeyAsBase58() + "?url=" + urlAsBase58);
+        assertEquals(server.getAddress(), created.getAddress());
+    }
+
     @Test
     public void testGetUrl() {
         assertSame(url, server.url);
+    }
+
+    @Test
+    public void testGetAddress() {
+        String address = server.getAddress();
+        assertTrue(address.matches("beam:[0-9a-zA-Z]{164}\\?url=[0-9a-zA-Z]{25}"));
+    }
+
+    @Test
+    public void testEquals() {
+        Server other = null;
+        assertFalse(server.equals(null));
+        assertFalse(server.equals(other));
+
+        other = Server.generate();
+        assertFalse(server.equals(other));
+
+        other.keyPair = EccKeyPairGenerator.fromPublicKey(server.getPublicKeyAsBytes());
+        assertFalse(server.equals(other));
+
+        other.url = server.url;
+        other.keyPair = EccKeyPairGenerator.fromBothKeys(server.getPublicKeyAsBytes(), server.getPrivateKeyAsBytes());
+        assertTrue(server.equals(other));
+
+        other.keyPair = server.keyPair;
+        assertTrue(server.equals(other));
+
+        other = new Server(server.url, keyPair);
+        assertTrue(server.equals(other));
+
+        other.url = null;
+        assertFalse(server.equals(other));
+
+        server.keyPair = EccKeyPairGenerator.fromPublicKey(keyPair.getPublic().getEncoded());
+        other.url = server.url;
+        other.keyPair = keyPair;
+        assertFalse(server.equals(other));
+
+        other.keyPair = EccKeyPairGenerator.fromPublicKey(keyPair.getPublic().getEncoded());
+        assertTrue(server.equals(other));
+
+        assertTrue(server.equals(server));
     }
 
     @Test
