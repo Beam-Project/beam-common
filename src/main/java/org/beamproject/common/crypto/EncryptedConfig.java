b72837b7a951cd2540a2f2d712a99af5ae8a4afd
René Bernhardsgrütter <rene.bernhardsgruetter@posteo.ch>
2014-07-20 21:39:43 +0200

    Replaced 'beam-client' and 'beam-server' in the license headers with 'beam-common'.

--- src/main/java/org/beamproject/common/crypto/EncryptedConfig.java
+++ src/main/java/org/beamproject/common/crypto/EncryptedConfig.java
@@ -1,14 +1,14 @@
 /*
  * Copyright (C) 2013, 2014 beamproject.org
  *
- * This file is part of beam-client.
+ * This file is part of beam-common.
  *
- * beam-client is free software: you can redistribute it and/or modify
+ * beam-common is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * beam-client is distributed in the hope that it will be useful,
+ * beam-common is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
