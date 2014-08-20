3a3260268914f78402f9b916ea32659f00d05d76
René Bernhardsgrütter <rene.bernhardsgruetter@posteo.ch>
2014-08-20 22:20:32 +0200

    Updated license (beam-server => beam-common).

--- src/main/java/org/beamproject/common/carrier/ClientCarrierModel.java
+++ src/main/java/org/beamproject/common/carrier/ClientCarrierModel.java
@@ -1,14 +1,14 @@
 /*
  * Copyright (C) 2013, 2014 beamproject.org
  *
- * This file is part of beam-server.
+ * This file is part of beam-common.
  *
- * beam-server is free software: you can redistribute it and/or modify
+ * beam-common is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * beam-server is distributed in the hope that it will be useful,
+ * beam-common is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
