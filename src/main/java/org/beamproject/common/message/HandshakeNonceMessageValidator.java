a3d3b3241a6e3465e75210bbfe37dbd095eac45e
René Bernhardsgrütter <rene.bernhardsgruetter@posteo.ch>
2014-08-22 03:27:31 +0200

    Created nonce validator for handshake since this is needed to validate incoming messages.

---
+++ src/main/java/org/beamproject/common/message/HandshakeNonceMessageValidator.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2013, 2014 beamproject.org
+ *
+ * This file is part of beam-common.
+ *
+ * beam-common is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * beam-common is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.beamproject.common.message;
+
+import org.beamproject.common.Message;
+import org.beamproject.common.MessageField;
+import static org.beamproject.common.MessageField.ContentField.*;
+import org.beamproject.common.crypto.EccKeyPairGenerator;
+import org.beamproject.common.crypto.Handshake;
+import static org.beamproject.common.crypto.Handshake.NONCE_LENGTH_IN_BYTES;
+
+/**
+ * Verifies that a {@link Message} contains a valid nonce used for a
+ * {@link Handshake}.
+ *
+ * @see Handshake
+ * @see EccKeyPairGenerator
+ */
+public class HandshakeNonceMessageValidator implements MessageValidator {
+
+    /**
+     * Verifies if the given message contains a valid nonce, stored under the
+     * key {@link MessageField.ContentField#HSNONCE}.
+     *
+     * @param message The message to verify.
+     * @return true, if a valid nonce is stored, false otherwise.
+     */
+    @Override
+    public boolean isValid(Message message) {
+        return message.containsContent(HSNONCE)
+                && message.getContent(HSNONCE) != null
+                && message.getContent(HSNONCE).length == NONCE_LENGTH_IN_BYTES;
+    }
+
+}
---
+++ src/test/java/org/beamproject/common/message/HandshakeNonceMessageValidatorTest.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2013, 2014 beamproject.org
+ *
+ * This file is part of beam-common.
+ *
+ * beam-common is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * beam-common is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.beamproject.common.message;
+
+import org.beamproject.common.Message;
+import static org.beamproject.common.MessageField.ContentField.HSNONCE;
+import static org.beamproject.common.crypto.Handshake.NONCE_LENGTH_IN_BYTES;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+public class HandshakeNonceMessageValidatorTest {
+
+    private HandshakeNonceMessageValidator validator;
+    private Message message;
+
+    @Before
+    public void setUp() {
+        validator = new HandshakeNonceMessageValidator();
+        message = new Message();
+    }
+
+    @Test
+    public void testIsValidOnMissingNonce() {
+        assertFalse(validator.isValid(message));
+    }
+
+    @Test
+    public void testIsValidOnNullNonce() {
+        message.getContent().put(HSNONCE.toString(), null);
+        assertFalse(validator.isValid(message));
+    }
+
+    @Test
+    public void testIsValidOnLength() {
+        for (int i = 0; i < NONCE_LENGTH_IN_BYTES * 2; i++) {
+            message.getContent().put(HSNONCE.toString(), getArrayOfLengt(i));
+
+            if (i == NONCE_LENGTH_IN_BYTES) {
+                assertTrue(validator.isValid(message));
+            } else {
+                assertFalse(validator.isValid(message));
+            }
+        }
+    }
+
+    private byte[] getArrayOfLengt(int length) {
+        byte[] bytes = new byte[length];
+
+        for (int i = 0; i < length; i++) {
+            bytes[i] = (byte) i;
+        }
+
+        return bytes;
+    }
+
+}
